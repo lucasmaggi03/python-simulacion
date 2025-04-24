979f44cc0e61544a03827be29b0e9a93e0727416
Mauricio Fiorini <95772574+MauricioFiorini@users.noreply.github.com>
2025-04-24 11:58:38 -0300

    cambios

--- tps/sim-ruleta-2/ruleta_v2.py
+++ tps/sim-ruleta-2/ruleta_v2.py
@@ -10,22 +10,26 @@ num_elegido = int(sys.argv[2])
 cant_tiradas = int(sys.argv[1])
 valores_corrida = []
 montos_cajas_corrida = []
+indice_cant_intentos_corrida = []
+frec_rel_cant_intentos_repetidos_corrida = []
 
 
 numeros_rojos = [1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]
-numeros_negros = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36]
 numeros_columnas = [[1,2,3,4,5,6,7,8,9,10,11,12],[13,14,15,16,17,18,19,20,21,22,23,24],[25,26,27,28,29,30,31,32,33,34,35,36]]
 numeros_filas = [[1,4,7,10,13,16,19,22,25,28,31,34],[2,5,8,11,14,17,20,23,26,29,32,35],[3,6,9,12,15,18,21,24,27,30,33,36]]
 
 
-fig, axs = plt.subplots(1, 2, figsize=(18, 6))
-axs[0].axhline(y=10000, color='red', linestyle='--', label="Monto de caja inicial")
+plt.axhline(y=10000, color='red', linestyle='--', label="Monto de caja inicial")
 for j in range(num_corridas):
     #Elegimos el monto caja inicial de 10000 U.M.
     monto_caja_actual = 10000
     valores = [random.randint(0,36) for i in range(cant_tiradas)]
     valores_corrida.append(valores)
     monto_caja = []
+    tiradas_para_ganar = []
+    frec_rel_cant_intentos_repetidos = []
+    indice_cant_intentos = []
+    cont_repeticiones = 1
 
 
     if (estrategia_elegida == "m"):
@@ -37,9 +41,12 @@ for j in range(num_corridas):
             if(valores[i] in numeros_rojos):
                 monto_caja_actual = monto_caja_actual + apuesta_m
                 apuesta_m = 10
+                tiradas_para_ganar.append(cont_repeticiones)
+                cont_repeticiones = 1
             else:
                 monto_caja_actual = monto_caja_actual - apuesta_m
                 apuesta_m = apuesta_m*2
+                cont_repeticiones = cont_repeticiones + 1
             monto_caja.append(monto_caja_actual)
             print(monto_caja)
 
@@ -60,11 +67,14 @@ for j in range(num_corridas):
                 if ((f1 - f2) >= 0):
                     f1 = f1 - f2
                 apuesta_f = f1 + f2
+                tiradas_para_ganar.append(cont_repeticiones)
+                cont_repeticiones = 1
             else:
                 monto_caja_actual = monto_caja_actual - apuesta_f
                 f1 = f2
                 f2 = apuesta_f
                 apuesta_f = f1 + f2
+                cont_repeticiones = cont_repeticiones + 1
             monto_caja.append(monto_caja_actual)
 
 
@@ -79,21 +89,71 @@ for j in range(num_corridas):
                 monto_caja_actual = monto_caja_actual + apuesta_d
                 if(apuesta_d > 10):
                     apuesta_d = apuesta_d - constante_suma_resta
+                tiradas_para_ganar.append(cont_repeticiones)
+                cont_repeticiones = 1
             else:
                 monto_caja_actual = monto_caja_actual - apuesta_d
                 apuesta_d = apuesta_d + constante_suma_resta
+                cont_repeticiones = cont_repeticiones + 1
             monto_caja.append(monto_caja_actual)
 
 
+    if  (estrategia_elegida == "x"):
+        # Apostamos a las filas y columnas contrarias del numero que salio
+        apuesta_x = 10
+        for i in range(cant_tiradas):
+            if(((monto_caja_actual - apuesta_x) < 0) and (tipo_capital == "f")):
+                break
+            if(i > 0):
+                for j in range(3):
+                    if valores[i-1] in numeros_columnas[j]:
+                        columna_anterior = j
+                    if valores[i-1] in numeros_filas[j]:
+                        fila_anterior = j
+                if((valores[i] not in numeros_columnas[columna_anterior]) and (valores[i] not in numeros_filas[fila_anterior])):
+                    monto_caja_actual = monto_caja_actual + (apuesta_x * 2)
+                    tiradas_para_ganar.append(cont_repeticiones)
+                    cont_repeticiones = 1
+                elif((valores[i] not in numeros_columnas[columna_anterior]) or (valores[i] not in numeros_filas[fila_anterior])):
+                    monto_caja_actual = monto_caja_actual - apuesta_x
+                    cont_repeticiones = cont_repeticiones + 1
+                else:
+                    monto_caja_actual = monto_caja_actual - (apuesta_x * 4)
+                    cont_repeticiones = cont_repeticiones + 1
+            monto_caja.append(monto_caja_actual)
+
+
+    cant_intentos_repetidos = []
+    for i in range(max(tiradas_para_ganar)):
+        indice_cant_intentos.append(i+1)
+        num = tiradas_para_ganar.count(i+1)
+        cant_intentos_repetidos.append(num/cant_tiradas)
+    sum_intentos_repetidos = sum(cant_intentos_repetidos)
+    for i in range(max(tiradas_para_ganar)):
+        frec_rel_cant_intentos_repetidos.append(cant_intentos_repetidos[i]/sum_intentos_repetidos)
+    print(frec_rel_cant_intentos_repetidos)
+    print(indice_cant_intentos)
+    frec_rel_cant_intentos_repetidos_corrida.append(frec_rel_cant_intentos_repetidos)
+    indice_cant_intentos_corrida.append(indice_cant_intentos)
+
     montos_cajas_corrida.append(monto_caja)
 
-    axs[0].plot(monto_caja, label=f"Frecuencia relativa de victorias en la corrida")
-    axs[0].set_xlabel('Número de tiradas')
-    axs[0].set_ylabel('Cantidad de capital')
-    axs[0].set_title(f'Flujo de caja')
+    plt.plot(monto_caja, label=f"Frecuencia relativa de victorias en la corrida")
+    plt.xlabel('Número de tiradas')
+    plt.ylabel('Cantidad de capital')
+    plt.title(f'Flujo de caja')
 
 plt.savefig("Graficas.png")
 plt.show()
+
+for i in range (num_corridas):
+    plt.figure(figsize=(8,6))
+    plt.bar(indice_cant_intentos_corrida[i], frec_rel_cant_intentos_repetidos_corrida[i], color='blue')
+    plt.xlabel('Numero de Tiradas')
+    plt.ylabel('Frecuencia Relativa')
+    plt.title(f'Frecuencia relativa en corrida: {(i+1)}')
+    plt.savefig(f'grafica_frecuencia_relativa_corrida{(i+1)}.png')        
+    plt.show()
 
 
 
